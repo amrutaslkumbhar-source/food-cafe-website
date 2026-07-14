e641036de25b14d60151ab961d12bb8dfff35b2b
Amruta Kumbhar <amrutaslkumbhar@gmail.com>
2026-07-14 14:13:49 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,125 @@
+
+const menu = [
+  {
+    id: 1,
+    category:"pizza",
+    name: "Margherita",
+    description: "Classic tomato sauce, fresh mozzarella, and organic basil.",
+    price: 200,
+    img:"images/marga.jpg"
+  },
+  {
+    id: 2,
+    category:"pizza",
+    name: "Pepperoni",
+    description: "Spicy pepperoni, mozzarella cheese, and crushed red pepper.",
+    price: 300,
+     img:"images/peppo.jpg"
+  },
+  {
+    id: 3,
+    category:"pizza",
+    name: "Veggie Supreme",
+    description: "Bell peppers, onions, mushrooms, black olives, and sweet corn.",
+    price: 350,
+     img:"images/veggsup.jpg"
+  },
+  {
+    id: 4,
+    category:"pizza",
+    name: "BBQ Chicken",
+    description: "Grilled chicken, smoky BBQ sauce, red onions, and cilantro.",
+    price: 400,
+     img:"images/BBQ-Chicken.jpg"
+  },
+   {
+    id: 5,
+    category:"burger",
+    name: "Cheeseburger",
+    description: "Flame-grilled beef patty, cheddar cheese, lettuce, tomato, and house sauce.",
+    price: 150,
+    img:"images/cheeb.jpg"
+      
+  },
+  {
+    id: 6,
+    category:"burger",
+    name: "Avocado Burger",
+    description: "Crispy bacon, fresh avocado, swiss cheese, and garlic aioli.",
+    price: 200,
+    img:"images/avo.jpg"
+  },
+  {
+    id: 7,
+    category:"burger",
+    name: "Crispy Chicken",
+    description: "Buttermilk fried chicken breast, spicy mayo, pickles, and pepper jack.",
+    price: 400,
+    img:"images/crispcb.jpg"
+  },
+    {
+    id: 8,
+    category:"pasta",
+    name: "Spaghetti",
+    description: " Long, thin cylindrical noodles. It is the most globally recognized shape and pairs well with smooth tomato sauces, olive oil, and light seafood.",
+    price: 200,
+    img:"images/spag.jpg"
+  },
+    {
+    id: 9,
+    category:"pasta",
+    name: "Penne",
+    description: " Short, tubular pasta with angled cut ends. The ridges trap chunky sauces, making it perfect for thick meat sauces and spicy arrabbiata.",
+    price: 200,
+    img:"images/penne.jpg"
+  },
+    {
+    id: 10,
+    category:"pasta",
+    name: "Farfalle",
+    description: "Known as bow-tie or butterfly pasta. It works well in pasta salads or with creamy, buttery sauces that coat all the edges.",
+    price: 200,
+    img:"images/farfale.jpg"
+  },
+    {
+    id: 11,
+    category:"pasta",
+    name: "Ravioli",
+    description: " Stuffed pasta filled with ingredients like ricotta, spinach, or meat. They are best served with light butter and sage sauces or delicate broths.",
+    price: 200,
+    img:"images/rav.jpg"
+  },
+  {
+    id: 12,
+    category:"dessert",
+    name: "Royal Rose Falooda",
+    description: "A tall glass layered with sweet basil seeds (sabja), thin cornstarch vermicelli (sev), heavy rose syrup, and chilled milk, topped with a scoop of vanilla or kulfi ice cream.",
+    price: 200,
+    img:"images/rose.jpg"
+  },
+  {
+    id: 13,
+    category:"dessert",
+    name: "Mango Falooda",
+    description: "layers of fresh mango puree, vanilla milk, vermicelli, and basil seeds, finished with mango ice cream and chopped pistachios.",
+    price: 200,
+    img:"images/mango.jpg"
+  },
+  {
+    id: 14,
+    category:"dessert",
+    name: "Kesar Pista Falooda",
+    description: "A rich, golden variant featuring milk infused with saffron (kesar) and crushed pistachios, layered with traditional sev and topped with authentic pistachio kulfi.",
+    price: 200,
+    img:"images/kesar.jpg"
+  },
+  {
+    id: 15,
+    category:"dessert",
+    name: "Chocolate Falooda",
+    description: "A modern twist using chocolate syrup, chocolate milk, and chocolate chip ice cream, while retaining the traditional basil seeds and vermicelli for texture.",
+    price: 200,
+    img:"images/chocklate.jpg"
+  }
+
+];
